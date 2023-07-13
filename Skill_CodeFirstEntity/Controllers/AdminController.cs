9aab33d32849645f3044bb615a0686510ebc42c8
Ebrar Temel <ebrartml2@gmail.com>
2023-07-13 23:20:45 +0300

    Add project files.

---
+++ Skill_CodeFirstEntity/Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Skill_CodeFirstEntity.Models.siniflar;
+namespace Skill_CodeFirstEntity.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context cn = new Context();
+
+        public ActionResult Index()
+        {
+            var degerler = cn.Yetenklers.ToList();
+
+            
+            return View(degerler); 
+        }
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(Yetenekler y)
+        {
+            cn.Yetenklers.Add(y);
+            cn.SaveChanges();
+            return View();
+        }
+        public ActionResult YetenekSil(int id)
+        {
+            var sil = cn.Yetenklers.Find(id);
+            cn.Yetenklers.Remove(sil);
+            cn.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekGuncelle(int id)
+        {
+            var guncellle = cn.Yetenklers.Find(id);
+           return View("YetenekGuncelle",guncellle);
+
+        }
+        [HttpPost]
+        public ActionResult YetenekGuncelle(Yetenekler y)
+        {
+            var guncelle = cn.Yetenklers.Find(y.Id);
+            guncelle.Deger = y.Deger;
+            guncelle.Aciklama = y.Aciklama;
+            cn.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
+    }
+}
---
+++ Skill_CodeFirstEntity/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Skill_CodeFirstEntity.Models.siniflar;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+
+namespace Skill_CodeFirstEntity.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            Models.siniflar.Context cn = new Models.siniflar.Context();
+            var deger = cn.Yetenklers.ToList();
+            return View(deger);
+        }
+    }
+}
